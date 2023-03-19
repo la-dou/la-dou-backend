417554ddc847531f9ec7bb316da56fabe14bd9c0
soomro-abd <24100180@lums.edu.pk>
2023-03-19 15:23:32 +0500

    added pydantic models for all entities

---
+++ app/models/driver.py
@@ -0,0 +1,9 @@
+from typing import List
+from pydantic import BaseModel
+from .rating import Rating
+from .order import DriverOrder
+
+class Driver(BaseModel):
+    deactivated: bool
+    rating: Rating
+    orders: List[DriverOrder]
---
+++ app/models/user.py
@@ -0,0 +1,32 @@
+from uuid import UUID
+from typing import List
+from pydantic import BaseModel, Field
+from .customer import Customer
+from .driver import Driver
+
+class UserAuth(BaseModel):
+    roll_no: int = Field(..., description="user roll number")
+    password: str = Field(..., min_length=5, max_length=24, description="user password")
+    role: str = Field(..., description="user role")
+
+class UserOut(BaseModel):
+    id: UUID
+    roll_no: int
+
+
+class SystemUser(UserOut):
+    role: str
+    password: str
+    
+class User(UserOut):
+    name: str
+    password: str
+    phone_number: str
+    gender: str
+    email_verified: bool
+    phone_verified: bool
+    fcm_registration_token: List[str]
+    customer: Customer
+    driver: Driver
+    
+    
