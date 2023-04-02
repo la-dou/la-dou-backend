33f342ae94949dd90342f141d4c029eadedbad7f
Saad Mehmoon <47217376+saadmehmoon@users.noreply.github.com>
2023-04-02 16:15:03 +0500

    LAD-72 fixed conflicts

--- app/main.py
+++ app/main.py
@@ -1,17 +1,10 @@
-from .config.database import db
-from .schemas.user import usersEntity
-from .routes.user import user
-from .routes.rating import ratings_router
-from typing import Union
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
 from .routes.auth import auth
 from .routes.otp import otp_router
 from .routes.fcm import fcm
-<< << << < Updated upstream
-== == == =
->>>>>> > Stashed changes
+from .routes.user import user
+from .config.database import db
 
 
 app = FastAPI()
@@ -31,7 +24,6 @@ app.include_router(otp_router, prefix="/otp",
 app.include_router(fcm, prefix="/fcm",
                    tags=["FCM (Firebase Cloud Messaging) Token Management"])
 app.include_router(user, tags=["User Management"])
-app.include_router(ratings_router, tags=["Ratings"])
 
 
 @app.on_event("startup")
