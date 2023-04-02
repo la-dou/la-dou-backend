8e5296c5828d03dd76c5e6993cd220861e8f7514
Zohaib <zohaib.khan3502@gmail.com>
2023-04-03 04:18:11 +0500

    Add default values for order model

--- app/models/order.py
+++ app/models/order.py
@@ -1,14 +1,14 @@
 from pydantic import BaseModel
 
 class BaseOrder(BaseModel):
-    deliver_to: str # where to deliver the order
-    deliver_from: str # where to pick up the order
-    notes: str # any additional notes
-    delivery_price: int # final accepted bid
-    order_amount: int # total amount of the order
-    placed_at: str # time when the order was placed
-    delivered_at: str # time when the order was delivered
-    status: str # status of the order
+    deliver_to: str = "" # where to deliver the order
+    deliver_from: str = "" # where to pick up the order
+    notes: str = "" # any additional notes
+    delivery_price: int = -1 # final accepted bid
+    order_amount: int = -1 # total amount of the order
+    placed_at: str = "" # time when the order was placed
+    delivered_at: str = "" # time when the order was delivered
+    status: str = "pending" # status of the order
     
 class CustomerOrder(BaseOrder):
     driver_roll_no: int # roll no of the driver who accepted the order
