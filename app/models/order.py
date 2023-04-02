814140dbfc7a9a49a7b46f4a3678d84bd582eb38
Zohaib <zohaib.khan3502@gmail.com>
2023-04-03 03:27:49 +0500

    Add endpoint for creating orders in the database and using global var in routes/order.py

--- app/models/order.py
+++ app/models/order.py
@@ -1,17 +1,17 @@
 from pydantic import BaseModel
 
 class BaseOrder(BaseModel):
-    by: str
-    to: str
-    notes: str
-    delivery_price: int
-    order_amount: int
-    placed_at: str
-    delivered_at: str
-    status: str
+    deliver_to: str # where to deliver the order
+    deliver_from: str # where to pick up the order
+    notes: str # any additional notes
+    delivery_price: int # final accepted bid
+    order_amount: int # total amount of the order
+    placed_at: str # time when the order was placed
+    delivered_at: str # time when the order was delivered
+    status: str # status of the order
     
 class CustomerOrder(BaseOrder):
-    driver_roll_no: int
+    driver_roll_no: int # roll no of the driver who accepted the order
     
 class DriverOrder(BaseOrder):
-    rider_roll_no: int
+    customer_roll_no: int # roll no of the customer who placed the order
