9b9f490b5ddfdf6847a1ad72128bb3e595274edd
Saad Mehmoon <47217376+saadmehmoon@users.noreply.github.com>
2023-04-05 14:24:26 +0500

    LAD-51 Added Token Auth for WS
    The token is sent as a part of the URL of websocket

--- app/routes/chat.py
+++ app/routes/chat.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, WebSocket, WebSocketDisconnect, Depends
+from fastapi import APIRouter, WebSocket, WebSocketDisconnect
 from ..utils.chat import manager
 from ..config.deps import get_current_user
 from ..config.database import db
@@ -6,11 +6,12 @@ from datetime import datetime
 
 chat = APIRouter()
 
-@chat.websocket("/chat/{user_id}/{second_user_id}")
-async def websocket_endpoint(websocket: WebSocket, user_id: int, second_user_id: int):
-    # fetch user from db
-    await manager.connect(websocket, user_id)
-    
+
+@chat.websocket("/chat/{token}/{second_user_id}")
+async def websocket_endpoint(websocket: WebSocket, token: str, second_user_id: int):
+    user = await get_current_user(token)
+    await manager.connect(websocket, user.roll_no)
+
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
     try:
@@ -18,5 +19,5 @@ async def websocket_endpoint(websocket: WebSocket, user_id: int, second_user_id:
             data = await websocket.receive_text()
             await manager.send_personal_message(data, second_user_id)
     except WebSocketDisconnect:
-        manager.disconnect(user_id)
-        await manager.send_personal_message(f"Client left the chat at {current_time}", second_user_id)
+        manager.disconnect(user.roll_no)
+        # await manager.send_personal_message(f"Client left the chat at {current_time}", second_user_id)
