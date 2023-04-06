fcd982bfe2f662388c46c33ce242b9de1e03898a
Saad Mehmoon <47217376+saadmehmoon@users.noreply.github.com>
2023-04-06 13:27:58 +0500

    LAD-100 Integrated FCM
    Also Added functions to send notifs via FCM

--- app/routes/fcm.py
+++ app/routes/fcm.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter, Depends
 from ..config.database import db
 from ..models.user import UserOut, SystemUser
 from ..config.deps import get_current_user
+from ..utils.fcm import send_notification, send_notification_to_user
 
 fcm = APIRouter()
 
@@ -31,3 +32,8 @@ async def remove_firebase_token(token: str, current_user: SystemUser = Depends(g
                     "$set": {"fcm_device_token": user["fcm_device_token"]}})
     # Return user
     return user
+
+# TODO: REMOVE THIS LATER
+@fcm.post('/test')
+async def send_notif(title: str, body: str, roll_no: int):
+    send_notification_to_user(title, body, roll_no)
