286ea8a9ea9604bf9c25f455e714004aa8c4a825
Saad Mehmoon <47217376+saadmehmoon@users.noreply.github.com>
2023-04-10 01:36:00 +0500

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -3,6 +3,6 @@
 Gunicorn with Uvicorn config to launch in Digital Ocean's App Platform.
 """
 bind = "0.0.0.0:8080"
-workers = 2
+workers = 1
 # Uvicorn's Gunicorn worker class
 worker_class = "uvicorn.workers.UvicornWorker"
