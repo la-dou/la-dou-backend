cf8afa486d5f8685bc0732163d88c4203b14cec1
muj2001 <m.usman.jehangir@gmail.com>
2023-04-24 20:57:34 +0500

    Root Test

--- app/tests/sample_test.py
+++ app/tests/sample_test.py
@@ -1,4 +1,12 @@
+import pytest
+import requests
+
+ENDPOINT = "http://localhost:8000"
 
 # Write a dummy test to make sure the test runner is working
-def test_dummy():
-    assert True
+def test_root():
+    response = requests.get(ENDPOINT)
+    print(response)
+    assert response.status_code == 200
+    data = response.json()
+    pass
